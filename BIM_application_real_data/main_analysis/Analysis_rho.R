27db7f1d4df9f467dd837a418e3e7e69f5d58795
vltnjrdn <valentina@jordan-team.de>
2024-03-02 02:14:54 +0100

    Update Analysis_rho.R

--- BIM_application_real_data/main_analysis/Analysis_rho.R
+++ BIM_application_real_data/main_analysis/Analysis_rho.R
@@ -6,69 +6,65 @@ library(ggpubr)
 library(rstatix)
 
 Analysis_rho <- function(all_conditions_list) {
-  
+
   ## repeated measures ANOVA for rho
+
   # Initialize an empty list to store the second columns
   rho_list <- list()
-  
+
   # Extract the first column from each data frame in the list
   for (df_name in names(all_conditions_list)) {
     rho_list[[df_name]] <- all_conditions_list[[df_name]][[4]]
   }
-  
+
   # Data preparation
   rho_df <- as.data.frame(rho_list)
-  
+
   rho_tibble <- rho_df %>%
     mutate(ID = row_number()) %>%
     select(ID, everything()) %>%
     as_tibble()
-  
+
   rho_data <- rho_tibble %>%
     gather(key = "condition", value = "rho", forward, backward, symmetrical) %>%
     convert_as_factor(ID, condition)
-  
+
   # Delete ID 23 (participant 29), for this participant all recall trials are the same
-  
   rho_data <- rho_data %>%
     filter(ID != 23 ) %>%
-    mutate(ID = rep(1:(length(ID)/3), length.out = n()))  
-  
+    mutate(ID = rep(1:(length(ID)/3), length.out = n()))
+
   # compute summary statistics
-  
   summary_stats <- rho_data %>%
     group_by(condition) %>%
     get_summary_stats(rho, type = "mean_sd")
-  
+
   ## Check assumptions
-  
+
   # check for outliers
-  
   outliers <- rho_data %>%
     group_by(condition) %>%
     identify_outliers(rho) # no extreme outliers found
-  
+
   # assumption of normality
-  
   normality <- rho_data %>%
     group_by(condition) %>%
     shapiro_test(rho)
-  
+
   # Perform ANOVA if normality assumptions hold, otherwise perform Friedman test
   if(all(normality$p > 0.05)) {
-    ## repeated measures ANOVA
-    
+
+    # repeated measures ANOVA
     res_aov <- anova_test(data = rho_data, dv = rho, wid = ID, within = condition)
     anova_table <- get_anova_table(res_aov)
-    
-    ## posthoc tests
-    
+
+    # posthoc tests
     pwc <- rho_data %>%
       pairwise_t_test(
         rho ~ condition, paired = TRUE,
         p.adjust.method = "bonferroni"
       ) # significant differences in rho when comparing the forward, backward and symmetrical condition to the unrelated condition
-    
+
     # Visualization: box plots with p-values
     bxp <- ggboxplot(rho_data, x = "condition", y = "rho", add = "point")
     pwc_bxp <- pwc %>% add_xy_position(x = "condition")
@@ -76,7 +72,7 @@ Analysis_rho <- function(all_conditions_list) {
       stat_pvalue_manual(pwc_bxp) +
       labs(caption = get_pwc_label(pwc_bxp)) +
       ggtitle(bquote(rho))
-    
+
     # Return results
     results <- list(summary_stats = summary_stats,
                     outliers = outliers,
@@ -85,24 +81,21 @@ Analysis_rho <- function(all_conditions_list) {
                     pairwise_tests = pwc,
                     test_used = "ANOVA",
                     plot = bxp)
-    
+
   } else {
-    ## Friedman Test
-    
+    # Friedman Test
     res.fried <- rho_data %>% friedman_test(rho ~ condition |ID)
-    
+
     # effect size
-    
     effect_size <- rho_data %>% friedman_effsize(rho ~ condition |ID)
-    
-    ## posthoc tests
-    
+
+    # posthoc tests
     pwc <- rho_data %>%
       pairwise_t_test(
         rho ~ condition, paired = TRUE,
         p.adjust.method = "bonferroni"
       ) # significant differences between every condition, except for symmetrical and forward
-    
+
     # Return results
     results <- list(summary_stats = summary_stats,
                     outliers = outliers,
@@ -112,6 +105,6 @@ Analysis_rho <- function(all_conditions_list) {
                     pairwise_tests = pwc,
                     test_used = "Friedman")
   }
-  
+
   return(results)
 }
